e3180aad2d499be218202ef8900d4ae3f515bcc9
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-03 17:00:56 -0400

    need to actually build

--- dist/rets_rabbit.umd.js
+++ dist/rets_rabbit.umd.js
@@ -1877,20 +1877,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	    url += domain;
 	
 	    //remove trailing slash from domain
-	    if(url.substr(url.length-1) === '/')
+	    if (url.substr(url.length - 1) === '/')
 	        url = url.slice(0, -1);
 	
-	    //remove leading slash
-	    if(target.substr(url.length-1) === '/')
-	        target = target.slice(0, -1);
-	
-	    //make sure target has leading slash
-	    if(target.substr(0) !== '/')
-	        target = '/' + target;
+	    //add forward slash to target
+	    if(target.substr(target.length - 1) !== '/')
+	        target += '/';
 	
 	    url += target;
-	    
-	    return url;
 	};
 
 
